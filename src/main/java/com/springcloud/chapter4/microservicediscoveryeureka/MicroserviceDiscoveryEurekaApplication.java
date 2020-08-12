e649ddcf8572d347154714a731481c4dd75fc42e
rabbit0523 <602120418@qq.com>
2020-08-12 23:16:14 +0800

    Initial commit

---
+++ src/main/java/com/springcloud/chapter4/microservicediscoveryeureka/MicroserviceDiscoveryEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud.chapter4.microservicediscoveryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//4.4编写Eureka Server
+//声明这是一个EurekaServer类
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroserviceDiscoveryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
+	}
+
+}
